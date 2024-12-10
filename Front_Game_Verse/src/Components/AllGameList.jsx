e0ab4055e8c9991d9e24f4434b8cfdc6b61f4269
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2024-12-10 13:40:23 -0300

    Created component and route GameList

--- Front_Game_Verse/src/Components/AllGameList.jsx
+++ Front_Game_Verse/src/Components/AllGameList.jsx
@@ -1,11 +1,22 @@
-export default function AllGameList({data}){
+import { useNavigate } from "react-router-dom"
 
+export default function AllGameList({data}){
+    const navigate = useNavigate();
+
+    function byList(listId, nameList){
+        const query = new URLSearchParams();
+        query.set("listId", listId);
+        query.set("nameList", nameList)
+        navigate(`/GameList?${query.toString()}`)
+    }
 
     return(
     <div className="w-[600px] h-[300px]  text-4xl font-bold grid justify-center">
             {data.map(gameList =>
                 <div key={gameList.id} className="relative top-[55px] left-[8px] justify-self-center">
-                    <button className="bg-orange-400 hover:bg-orange-500 text-stone-700 rounded-[50px] p-[25px] m-[10px]">
+                    <button 
+                    onClick={() => byList(gameList.id, gameList.name)}
+                    className="bg-orange-400 hover:bg-orange-500 text-stone-800 rounded-[50px] p-[25px] m-[10px]">
                         {gameList.name}
                     </button>
                 </div>
---
+++ Front_Game_Verse/src/Components/GameList.jsx
@@ -0,0 +1,10 @@
+
+function GameList(){
+    return(
+        <div>
+            <h1> TESTE GAME LIST</h1>
+        </div>
+    )
+}
+
+export default GameList;
